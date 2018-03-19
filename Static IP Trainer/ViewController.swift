6c1c3ddad3e868b870196fb83af029ebe7c1b862
Justin Kumpe <jakumpe@kumpes.com>
2018-03-18 21:02:53 -0500

    Initial Commit

---
+++ Static IP Trainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Static IP Trainer
+//
+//  Created by Justin Kumpe on 3/18/18.
+//  Copyright © 2018 Justin Kumpe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
